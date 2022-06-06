3c81251e78643b247704f45b9e22e0e669b2c0f7
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-06-06 14:49:09 +0300

    your message

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Write your code in this file!
+function scuberGreetingForFeet(someDistance) {
+//// Write your code here!
+    if (someDistance <= 400){
+            return "This one is on me!";
+    }
+    else if (someDistance > 2000 && someDistance < 2500){
+        return "I will gladly take your thirty bucks.";
+    }
+    else if(someDistance > 2500){
+        return "No can do.";
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+//// Write your code here!
+    if (city === "NYC") {
+        return "Ok, sounds good.";
+    }
+    else{
+        return "No go.";
+    }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+//// Write your code here!
+    if (tip === "generous") {
+        return "Thank you so much.";
+    } else if (tip === "not as generous") {
+        return "Thank you.";
+    } else {
+        return "Bye.";
+    }
+
+
+
+
 }
+
+
+
